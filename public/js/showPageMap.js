f6df2f458f92874c6371ced36c901272e7cb0c92
antleemoore <antleemoore@hotmail.com>
2021-01-21 17:21:47 -0500

    fixed session error with campgrounds generated by db

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -10,7 +10,7 @@ new mapboxgl.Marker()
    .setLngLat(campground.geometry.coordinates)
    .setPopup(
       new mapboxgl.Popup({ offset: 25 }).setHTML(
-         `<h4>${campground.title}</h4><p>${campground.location}</p>`
+         `<h4 id="campMapTitle">${campground.title}</h4><p>${campground.location}</p>`
       )
    )
    .addTo(map);
